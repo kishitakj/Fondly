d999d15763bd32c32f271e7d4b6b64d2f000d48f
“Kishita <kishitakj139@gmail.com>
2021-04-20 17:53:32 +0530

    first commit

---
+++ app1.js
@@ -0,0 +1,29 @@
+
+
+var budgetcontroller=(function(){
+    
+
+    }
+
+
+
+
+
+
+)();
+var UIcontroller=(function(){
+
+
+
+})();
+var controller=(function(budgetctrl, UIctrl){
+
+    document.querySelector('.add__btn').addeventlistener('click', function(){
+
+        console.log('button is clicked')
+});
+
+
+
+
+})(budgetcontroller, UIcontroller);
